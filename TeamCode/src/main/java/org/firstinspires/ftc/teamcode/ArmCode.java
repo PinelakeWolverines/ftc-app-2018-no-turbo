196362f0d6ad355bffa8638c369e8bc081623bf2
Navee <challanaveen03@gmail.com>
2018-12-11 15:49:38 -0800

    Gamepad Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmCode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;.robotcore.hardware.Servo;
+public class ArmCode extends LinearOpMode {
+    DcMotor armBase;
+    DcMotor armJoint;
+    DcMotor extender;
+    Servo intake;
+    Servo dumper;
+    Servo
+    public void runOpMode(){
+        if (gamepad1.left_bumper == true){
+
+        }
+
+    }
+}
