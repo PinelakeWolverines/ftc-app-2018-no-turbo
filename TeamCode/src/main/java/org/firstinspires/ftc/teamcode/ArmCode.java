7f688f116c3e16eb3a35b3b48038d3534ec1dbb8
Navee <challanaveen03@gmail.com>
2018-12-11 17:12:58 -0800

    All lines of code for arm have been added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmCode.java
@@ -2,18 +2,41 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Servo;
 public class ArmCode extends LinearOpMode {
     DcMotor armBase;
     DcMotor armJoint;
     DcMotor extender;
     Servo intake;
     Servo dumper;
-    Servo
+    Servo outtake;
+    public void cutPower(){
+        armJoint.setPower(0);
+        armBase.setPower(0);
+        extender.setPower(0);
+    }
     public void runOpMode(){
-        if (gamepad1.left_bumper == true){
-
-        }
-
+        double position = 0;
+        while (opModeIsActive())
+            while (gamepad1.left_bumper == true){
+                armBase.setPower(-0.6);
+                armJoint.setPower(-0.6);
+                dumper.setPosition(position+0.1);
+            }
+            cutPower();
+            while (gamepad1.left_trigger == 1){
+                armBase.setPower(0.6);
+                armJoint.setPower(0.6);
+            }
+            cutPower();
+            while (gamepad1.dpad_down == true){
+                intake.setPosition(0.5);
+                outtake.setPosition(0.5);
+            }
+            cutPower();
+            while (gamepad1.right_bumper == true){
+                extender.setPower(0.7);
+            }
+            cutPower();
     }
 }
