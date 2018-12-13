0415883b298a33b1a17a9501fe9a63d8b40929a0
Navee <challanaveen03@gmail.com>
2018-12-12 21:39:50 -0800

    ArmCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmCode.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+@TeleOp
 public class ArmCode extends LinearOpMode {
     DcMotor armBase;
     DcMotor armJoint;
@@ -15,13 +17,28 @@ public class ArmCode extends LinearOpMode {
         armBase.setPower(0);
         extender.setPower(0);
     }
+    public void initEncoder(){
+        armBase.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armJoint.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armBase.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armJoint.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
     public void runOpMode(){
+        armBase = hardwareMap.dcMotor.get("armBase");
+        armJoint = hardwareMap.dcMotor.get("armBase");
+        extender = hardwareMap.dcMotor.get("extender");
+        intake = hardwareMap.servo.get("intake");
+        outtake = hardwareMap.servo.get("outtake");
+        dumper = hardwareMap.servo.get("dumper");
         double position = 0;
         while (opModeIsActive())
             while (gamepad1.left_bumper == true){
-                armBase.setPower(-0.6);
-                armJoint.setPower(-0.6);
-                dumper.setPosition(position+0.1);
+                initEncoder();
+                double armBaseCurrPosition = armBase.getCurrentPosition();
+                double armJointCurrPosition = armJoint.getCurrentPosition();
+                armBase.setPower(-0.8);
+                armJoint.setPower(-0.4);
+                dumper.setPosition(position+0.01);
             }
             cutPower();
             while (gamepad1.left_trigger == 1){
